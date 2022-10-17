aaa5df70f1386b068e0fc00eac02cf714c5bc625
nadirbasalamah <nadir008basalamah@gmail.com>
2022-10-17 19:58:47 +0700

    chore(db): change db config for deployment

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/labstack/echo/v4"
@@ -30,11 +31,11 @@ func main() {
 	var err error
 
 	var dsn string = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True",
-		"root",
-		"",
-		"localhost",
-		"3306",
-		"echo_sample",
+		os.Getenv("DB_USERNAME"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
 	)
 
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
